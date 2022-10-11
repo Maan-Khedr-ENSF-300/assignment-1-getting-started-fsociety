9c93a02d45797e046b83db497e5d0749a86fb9b9
rhishikr <rhishik.royninabh@ucalgary.ca>
2022-10-10 20:34:28 -0600

    Added validation function
    Validation function for integers has been added.

--- Calculator.py
+++ Calculator.py
@@ -10,3 +10,10 @@ def myMul(num1, num2):
 def myDiv(num1, num2):
     return num1//num2
 
+def intVal():   
+   myIntegers = []
+ 
+   integer_number = 4
+   while len(myIntegers) < integer_number:
+       try:   
+           int_1 = int(input('Enter the first integer for your calculation: '))
