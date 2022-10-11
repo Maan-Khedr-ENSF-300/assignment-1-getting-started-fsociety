c8ac0dc0c3e782db6ddb96de3143c59fa8b0984b
aasha25 <afsana.asha@ucalgary.ca>
2022-10-10 22:04:46 -0600

    Added myCalc function
    Assigns values of the integers and operators in their respective positions according to the user's inputs.

--- Calculator.py
+++ Calculator.py
@@ -73,6 +73,14 @@ def operationVal():
   
    return myOperators
 
+def myCalc(myOperators, myIntegers):
+    first_op = myOperators[0]
+    second_op = myOperators[1]
+    first_int = myIntegers[0]
+    second_int = myIntegers[1]
+    third_int = myIntegers[2]
+    
+
 
   
 
