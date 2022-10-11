0b67c5113b14a7b3f31e34f9124781227d5b03e0
aasha25 <afsana.asha@ucalgary.ca>
2022-10-10 20:34:26 -0600

    Added myCalc function
    Assigns variable names to the different index number values of the lists that hold the user's integers and operators.

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,6 @@
-def myAdd(num1,num2):
-    return num1 + num2
-
-def mySub(num1, num2):
-    return num1 - num2
+def myCalc(myOperators, myIntegers):
+    first_op   = myOperators[0]
+    second_op  = myOperators[1]
+    first_int  = myIntegers[0]
+    second_int = myIntegers[1]
+    third_int  = myIntegers[2]
