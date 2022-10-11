0e139dd765097c6aebf0105a7ddd1b57927e48d5
kaniz17 <kaniz.taiba@ucalgary.ca>
2022-10-10 23:20:13 -0400

    Created a function and two lists for the operators
    Created two lists- one for the user input and another that specifies the operators that needs to be entered.

--- Calculator.py
+++ Calculator.py
@@ -44,3 +44,9 @@ def intVal():
            print('You entered: ', int_3)
        myIntegers.append(int_3)
        break
+   
+def operationVal():
+   myOperators = []
+   myOplist = [ '*' , '/' , '+' , '-' ]
+ 
+   print('Enter the operators that you would like to use: \n"*" for Multiplication\n"/" for Division\n"+" for Addition\n"-" for Subtration')
